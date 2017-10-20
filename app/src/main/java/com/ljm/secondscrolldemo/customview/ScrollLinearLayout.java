ad081cf6912a9fc86fa778eef64c78b6e0961c7f
bigMing <liujm2017@163.com>
2017-10-20 17:39:18 +0800

    first commit

--- app/src/main/java/com/ljm/secondscrolldemo/customview/ScrollLinearLayout.java
+++ app/src/main/java/com/ljm/secondscrolldemo/customview/ScrollLinearLayout.java
@@ -1,15 +1,32 @@
 package com.ljm.secondscrolldemo.customview;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.animation.PathInterpolator;
 import android.widget.LinearLayout;
+
+import com.ljm.secondscrolldemo.utills.ScrollStateValue;
 
 /**
  * Created by ljm on 2017/10/20.
  */
 
 public class ScrollLinearLayout extends LinearLayout {
+
+    private int mContentViewState = ScrollStateValue.LISTVIEW_TOP_STATE;
+    private ValueAnimator mTransitionAnim;
+    private boolean mIsAnimPlay = false;
+    private VelocityTracker mVelocityTracker;
+
+    private PathInterpolator easeOutCubic;
+    private PathInterpolator easeInCubic;
+
+    private int mLayoutHeightMin = 200;
+    private int mLayoutHeightMax = 400;
+    private int mLayoutThresHold = 300;
 
     public ScrollLinearLayout(Context context) {
         this(context, null);
@@ -25,11 +42,24 @@ public class ScrollLinearLayout extends LinearLayout {
     }
 
     private void init() {
-
+        easeOutCubic = new PathInterpolator(0.215f, 0.61f, 0.355f, 1);
+        easeInCubic = new PathInterpolator(0.55f, 0.055f, 0.675f, 0.19f);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        //如果动画在播放或ListView在滑动时，不处理事件
+        if (mIsAnimPlay
+                || mContentViewState == ScrollStateValue.LISTVIEW_FLING_STATE) {
+            return false;
+        }
+        //如果
+        if (mVelocityTracker == null) {
+            mVelocityTracker = VelocityTracker.obtain();
+        }
+        mVelocityTracker.addMovement(event);
+        final VelocityTracker vt = mVelocityTracker;
+
         return super.onTouchEvent(event);
     }
 }
---
+++ app/src/main/java/com/ljm/secondscrolldemo/utills/ScrollStateValue.java
@@ -0,0 +1,24 @@
+package com.ljm.secondscrolldemo.utills;
+
+/**
+ * Created by ljm on 2017/10/20.
+ * ScrollLinearLayout的状态值
+ */
+
+public class ScrollStateValue {
+
+    //ListView滑到顶部
+    public static final int LISTVIEW_TOP_STATE = 0;
+    //ListView在滑动
+    public static final int LISTVIEW_FLING_STATE = 1;
+    //ListView滑到底部
+    public static final int LISTVIEW_BOTTOM_STATE = 2;
+    //滚动的最大高度
+    public static final int SCROLL_MAX_STATE = 100;
+    //滚动的最小高度
+    public static final int SCROLL_MIN_STATE = 0;
+    //平移的最大的持续时间
+    public static final int TRANSITION_DURATION_MAX = 300;
+    //滚动的速阀值
+    public static final int SCROLL_VELOCITY_THRESHOLD = 300;
+}
